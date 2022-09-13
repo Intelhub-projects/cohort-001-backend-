7305641b206a705b0892ab2e38c7cf578b2dc623
Abass Adebayo A <greatmoh007@gmail.com>
2022-09-13 16:05:13 +0100

    ImplementationOfReminderSms

---
+++ Core/Appliaction/Constants/SmsReminderConstant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Appliaction.Constants
+{
+    public class SmsReminderConstant
+    {
+        public const string SuccessMessage = "Sms sent successfully";
+        public const string FailedMessage = "Failure in sending Sms";
+    }
+}
