5c8c790213f05411a7e1cff25f6de394f8a49e9d
Abass Adebayo A <greatmoh007@gmail.com>
2022-09-16 11:20:12 +0100

    email.workin....

--- Infrastructure/SendMail/MailService.cs
+++ Infrastructure/SendMail/MailService.cs
@@ -2,6 +2,9 @@
 using Application.Interfaces.Services;
 using Core.Appliaction.Interfaces.Services;
 using Core.Domain.Entities;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
 using System;
 using System.Collections.Generic;
@@ -13,51 +16,21 @@ namespace Infrastructure.SendMail
 {
     public class MailService : IMailService
     {
+        private readonly IConfiguration _configuration;
+        public string _mailKey;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _mailKey = _configuration.GetSection("MailConfiguration")["mailKey"];
+        }
+
         public async Task<BaseResponse> SendWelcomeMailToNewPatient(string recipientName, string recipientMail, string text)
         {
-            while (true)
-            {
-                try
-                {
-                  
-                    var message = new MimeMessage();
-                    message.From.Add(new MailboxAddress(
-                        "INTELHUB MEDPHARM CARE",
-                        "greatmoh007@gmail.com"
-                    ));
 
-                    message.To.Add(new MailboxAddress(
-                       $"{recipientName}",
-                        $"{recipientMail}"
-                    ));
-                    message.Subject = "Because Your Life Matters";
-                    var bodyBuilder = new BodyBuilder
-                    {
-                        TextBody = $"{text}",
-                        HtmlBody = "Believe in We, <b>IntelHub MedPharm Care</b>"
-                    };
-                    message.Body = bodyBuilder.ToMessageBody();
 
-                    using var client = new SmtpClient();
-                    // SecureSocketOptions.StartTls force a secure connection over TLS
-                    await client.ConnectAsync("smtp.sendgrid.net", 587, SecureSocketOptions.StartTls);
-                    await client.AuthenticateAsync(
-                        userName: "apikey", // the userName is the exact string "apikey" and not the API key itself.
-                        password: Environment.GetEnvironmentVariable("SENDGRID_API_KEY") // password is the API key
-                    );
 
-                    Console.WriteLine("Sending email");
-                    await client.SendAsync(message);
-                    Console.WriteLine("Email sent");
 
-                    await client.DisconnectAsync(true);
-                }
-                catch (Exception)
-                {
 
-                }
-            }
-            
         }
-    }
-}
+    }   }
---
+++ WebApi/Controllers/MailController.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTOs;
+using Core.Appliaction.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using RestfulAPI.Filters;
+using System.Net;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController: ControllerBase
+    {
+        private readonly IMailService _mailService;
+
+        public MailController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        [HttpPost("SendWelcomeMailToNewPatient")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ValidationResultModel))]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(BaseResponse))]
+        public async Task<IActionResult> SendWelcomeMailToNewPatient([FromBody] string recipientName, string recipientMail, string text)
+        {
+            var response = await _mailService.SendWelcomeMailToNewPatient(recipientName, recipientMail, text);
+            return Ok(response);
+        }
+    }
+}
