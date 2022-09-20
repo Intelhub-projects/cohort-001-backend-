9acd242411967d5c078ab90a32667f2130970b52
Abass Adebayo A <greatmoh007@gmail.com>
2022-09-20 10:25:09 +0100

    commit....

---
+++ Core/Appliaction/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Appliaction.Interfaces.Services
+{
+    internal interface IEmailService
+    {
+    }
+}
