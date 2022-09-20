919d3eb5562725f8c4499f00198e70a8dde376c8
Abass Adebayo A <greatmoh007@gmail.com>
2022-09-20 10:29:32 +0100

    Waiting...

--- Core/Appliaction/Interfaces/Services/IMailService.cs
+++ Core/Appliaction/Interfaces/Services/IMailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Core.Appliaction.Interfaces.Services
 {
-    internal interface IEmailService
+    public interface IMailService
     {
+        public void SendEmail(MailRequest mailRequest);
+
     }
 }
